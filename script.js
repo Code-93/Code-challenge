5b0d0b4bd84c6f293b92905f8eee91ec31a591bf
Code-93 <140856079+Code-93@users.noreply.github.com>
2023-10-01 10:27:52 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector("#name").value;
+  const surname = document.querySelector("#surname").value;
+  const birthdate = document.querySelector("#birthdate").value;
+
+  const data = {
+    name: name,
+    surname: surname,
+    birthdate: birthdate,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/users", options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
